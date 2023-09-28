cce6313c6c8c0a59cb38738a4db4c9a9e3485086
devRyan <ryan.kinuthia@student.moringaschool.com>
2023-09-28 17:23:56 +0300

    functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+function saturdayFun() {
+    console.log("This Saturday, I want to roller-skate!")
+}
+saturdayFun();
+//=> "This Saturday, I want to roller-skate"
+
+1 + 1; //=> 2
+"Razzle " + "dazzle!"; //=>"Razzle dazzle"
+
+(function() {
+    console.log("Yet more razzling");
+});
+
+const fn = function mondayWork () {
+    console.log(This mondayWork, I will go to the office)
+}
+
+function saturdayFun() {
+    const part1 = "roller skate";
+    return function () {
+        const part 2 = "go to office";
+        return function () {
+            console.log(`${part1.toUpperCase()} ${part2} ${saturdayFun}`);
+        }
+    }  
+}
